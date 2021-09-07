65aaeae0be9a803ef2e339b187e19aa8fb16cb90
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-09-07 14:56:24 +0300

    [refactor] root files

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
-import { Counter } from './features/counter/Counter';
+import { Suspense } from 'react';
+import { Switch } from 'react-router-dom';
+
+// import ProtectedRoute from 'common/components/ProtectedRoute';
+import Spinner from 'common/components/Spinner';
+import WithAuthGuard from 'features/auth/components/WithAuthGuard';
+import routes from 'router/index';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit
-          <code>src/App.js</code> and save to reload. <span>123</span>
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Suspense fallback={Spinner}>
+        <Switch>
+          {routes.map(route => (
+            <WithAuthGuard key={route.name} {...route} />
+          ))}
+        </Switch>
+      </Suspense>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { AuthProvider } from 'features/auth/auth.context';
 
 import App from './App';
 import { store } from './store/store';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
